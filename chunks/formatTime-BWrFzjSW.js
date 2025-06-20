5393cd82e7e4297c4f3f745f57ea06b1240911ca
edwin-ortizp <edwin.ortizp@campusucc.edu.co>
2025-06-20 12:52:42 -0500

    Build actualizado 2025-06-20T17-52-42-647Z

--- chunks/formatTime-BWrFzjSW.js
+++
@@ -1,1 +0,0 @@
-const n=o=>{const r=Math.floor(o/60),t=o%60;return`${r<10?"0":""}${r}:${t<10?"0":""}${t}`},a=o=>{if(o<60)return`${o} min`;const r=Math.floor(o/60),t=o%60;return`${r}h ${t}min`};export{n as a,a as f};
