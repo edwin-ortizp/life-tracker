53cfec48e17ea19ceeb7e843d9edbf41367f9192
edwin-ortizp <edwin.ortizp@campusucc.edu.co>
2025-07-04 12:30:56 -0500

    Build actualizado 2025-07-04T17-30-56-773Z

--- chunks/skeleton-BH6YhNuy.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./ui-vendor-DliCpqNh.js";import{e as o}from"../assets/index-pcBukfwd.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
---
+++ chunks/skeleton-oA1L0blK.js
@@ -0,0 +1,1 @@
+import{j as m}from"./ui-vendor-DliCpqNh.js";import{e as o}from"../assets/index-Be0NmGXk.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
