efb3356928438cae415146100ffb5bf1ce52e922
edwin-ortizp <edwin.ortizp@campusucc.edu.co>
2025-07-05 12:20:55 -0500

    Build actualizado 2025-07-05T17-20-55-038Z

--- chunks/skeleton-CeteuMue.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./ui-vendor-f3qfKfYu.js";import{e as o}from"../assets/index-DETJ-bwv.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
