3f20713de64f9004c60ee9d187ad3ed5a6b48981
edwin-ortizp <edwin.ortizp@campusucc.edu.co>
2025-07-09 09:55:34 -0500

    Build actualizado 2025-07-09T14-55-34-663Z

---
+++ chunks/skeleton-BBRCeEXk.js
@@ -0,0 +1,1 @@
+import{j as m}from"./ui-vendor-BLQH548t.js";import{e as o}from"../assets/index-CdZc1nOU.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
